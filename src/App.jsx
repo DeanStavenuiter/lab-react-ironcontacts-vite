b0abe088ab6b42a578011cc1e1a7bfc5606160df
DeanStavenuiter <ddstavenuiter@gmail.com>
2023-02-21 19:00:42 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import Contacts from "./contacts.json";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [firstSix, setfirstSix] = useState(Contacts.slice(0, 6));
+  const [remContacts, setRemContacts] = useState(Contacts.slice(7));
+
+  function getRandom() {
+    let randomIndex = Math.floor(Math.random() * remContacts.length);
+    let randomContact = remContacts[randomIndex];
+
+    setfirstSix([randomContact, ...firstSix]);
+    let filterArr = remContacts.filter((contact) => {
+      if (contact.name !== randomContact.name) {
+        return contact;
+      }
+    });
+    setRemContacts(filterArr);
+  }
+
+  function sortByName() {
+    const sort = firstSix.sort((a, b) => a.name.localeCompare(b.name));
+    setfirstSix([...sort]);
+  }
+
+  function sortByPopularity() {
+    const sort = firstSix.sort((a, b) => a.popularity - b.popularity);
+    setfirstSix([...sort]);
+  }
+
+  function deleteContact(id) {
+    const filteredContacts = firstSix.filter(contact => contact.id !== id);
+    setfirstSix(filteredContacts);
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>IronContacts</h1>
+      <button onClick={getRandom}>Random contacts</button>
+      <button onClick={sortByName}>Sort by name</button>
+      <button onClick={sortByPopularity}>Sort by popularity</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {firstSix.map((celeb) => {
+            return (
+              <tr>
+                <td>
+                  <img
+                    style={{ height: "150px" }}
+                    src={celeb.pictureUrl}
+                    alt="person"
+                  />
+                </td>
+                <td>{celeb.name}</td>
+                <td>{celeb.popularity}</td>
+                <td>{celeb.wonOscar ? "🏆" : ""}</td>
+                <td>{celeb.wonEmmy ? "🏆" : ""}</td>
+                <button onClick={() => deleteContact(celeb.id)}>delete</button>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-  )
-}
-
-export default App
+  );
+};
+export default App;
